5f711a8b69214b8046b378a47be2e03d08b48256
5420 <olllegborisov@gmail.com>
2024-03-20 18:56:19 +0300

    some sections were added

--- js/main.js
+++ js/main.js
@@ -36,3 +36,55 @@ function burger() {
 if (window.matchMedia("(max-width: 767px)").matches) {
     burger()
 }
+
+if (window.matchMedia("(max-width: 767px)").matches) {
+    function initWishSwiper() {
+        new Swiper(".services__swiper", {
+            autoplay: true,
+            loop: true,
+            centeredSlides: true,
+            slidesPerView: 1.4,
+            spaceBetween: 20,
+            fadeEffect: {
+                crossFade: true,
+            },
+        });
+    }
+    initWishSwiper()
+}
+
+
+
+
+
+document.querySelectorAll('.faq__question').forEach(item => {
+    item.addEventListener('click', () => {
+
+        const arrow = item;
+        const content = item.nextElementSibling;
+
+
+        if (content.style.maxHeight) {
+            document.querySelectorAll('.faq__text').forEach(item => {
+                item.style.maxHeight = null;
+                item.style.opacity = null;
+            })
+            document.querySelectorAll('.faq__question').forEach(item => {
+                item.classList.remove('_active');
+            })
+        } else {
+            document.querySelectorAll('.faq__text').forEach(item => {
+                item.style.maxHeight = null;
+                item.style.opacity = null;
+            })
+            content.style.maxHeight = content.scrollHeight + 'px';
+            content.style.opacity = 1;
+
+            document.querySelectorAll('.faq__question').forEach(item => {
+                item.classList.remove('_active');
+            })
+            arrow.classList.add('_active');
+        }
+    })
+})
+
